23bdd1751b7d7da2e476272fad5a116abf205e7a
Muhammad Zohaib <zohaib10092001@gmail.com>
2023-02-07 01:53:21 +0500

    PKDR contract token initialized

--- lambda/v2/pkdr_lambda/pkdr/contracts/PKDR.sol
+++ lambda/v2/pkdr_lambda/pkdr/contracts/PKDR.sol
@@ -8,35 +8,24 @@ import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/draft-ERC20PermitUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20VotesUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20FlashMintUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
-/// @custom:security-contact zohaib10092001@gmail.com
-contract PKDR is
-    Initializable,
-    ERC20Upgradeable,
-    ERC20BurnableUpgradeable,
-    ERC20SnapshotUpgradeable,
-    OwnableUpgradeable,
-    PausableUpgradeable,
-    ERC20PermitUpgradeable,
-    ERC20VotesUpgradeable,
-    ERC20FlashMintUpgradeable
-{
+contract PKDR is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable, ERC20SnapshotUpgradeable, OwnableUpgradeable, PausableUpgradeable, ERC20PermitUpgradeable, ERC20VotesUpgradeable, UUPSUpgradeable {
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
         _disableInitializers();
     }
 
-    function initialize() public initializer {
+    function initialize() initializer public {
         __ERC20_init("PKDR", "PKDR");
         __ERC20Burnable_init();
         __ERC20Snapshot_init();
         __Ownable_init();
         __Pausable_init();
-        __ERC20Permit_init("PKDR");
+        __ERC20Permit_init("MyToken");
         __ERC20Votes_init();
-        __ERC20FlashMint_init();
+        __UUPSUpgradeable_init();
     }
 
     function snapshot() public onlyOwner {
@@ -55,39 +44,40 @@ contract PKDR is
         _mint(to, amount);
     }
 
-    function _beforeTokenTransfer(
-        address from,
-        address to,
-        uint256 amount
-    )
+    function _beforeTokenTransfer(address from, address to, uint256 amount)
         internal
-        override(ERC20Upgradeable, ERC20SnapshotUpgradeable)
         whenNotPaused
+        override(ERC20Upgradeable, ERC20SnapshotUpgradeable)
     {
         super._beforeTokenTransfer(from, to, amount);
     }
 
+    function _authorizeUpgrade(address newImplementation)
+        internal
+        onlyOwner
+        override
+    {}
+
     // The following functions are overrides required by Solidity.
 
-    function _afterTokenTransfer(
-        address from,
-        address to,
-        uint256 amount
-    ) internal override(ERC20Upgradeable, ERC20VotesUpgradeable) {
+    function _afterTokenTransfer(address from, address to, uint256 amount)
+        internal
+        override(ERC20Upgradeable, ERC20VotesUpgradeable)
+    {
         super._afterTokenTransfer(from, to, amount);
     }
 
-    function _mint(
-        address to,
-        uint256 amount
-    ) internal override(ERC20Upgradeable, ERC20VotesUpgradeable) {
+    function _mint(address to, uint256 amount)
+        internal
+        override(ERC20Upgradeable, ERC20VotesUpgradeable)
+    {
         super._mint(to, amount);
     }
 
-    function _burn(
-        address account,
-        uint256 amount
-    ) internal override(ERC20Upgradeable, ERC20VotesUpgradeable) {
+    function _burn(address account, uint256 amount)
+        internal
+        override(ERC20Upgradeable, ERC20VotesUpgradeable)
+    {
         super._burn(account, amount);
     }
 }

07788d59a60b302feb097a39ed9783bf120b66c1
Muhammad Zohaib <zohaib10092001@gmail.com>
2023-03-20 01:36:14 +0500

    new sS added

--- lambda/v2/pkdr_lambda/src/index.ts
+++ lambda/v2/pkdr_lambda/src/index.ts
@@ -2,6 +2,10 @@ import mintPKDR from "../pkdr/pkdrAPI/api/mutation/mintPKDR/mintPKDR";
 import setProfileAddress from "../pkdr/pkdrAPI/api/mutation/setProfileAddress/setProfileAddress";
 import getProfileAddress from "../pkdr/pkdrAPI/api/query/getProfileAddress/getProfileAddress";
 import approve from "../pkdr/pkdrAPI/api/mutation/approve/approve";
+import getPlatFormFee from "../pkdr/pkdrAPI/api/query/getPlatFormFee/getPlatFormFee";
+import setPlatFormFee from "../pkdr/pkdrAPI/api/mutation/setPlatformFee/setPlatformFee";
+import transfer from "../pkdr/pkdrAPI/api/mutation/transfer/transfer";
+import transferFrom from "../pkdr/pkdrAPI/api/mutation/transferFrom/transferFrom";
 
 exports.handler = async (event: any, context: any) => {
   switch (event.info.fieldName) {
@@ -15,10 +19,21 @@ exports.handler = async (event: any, context: any) => {
     case "setProfileAddress":
       return await setProfileAddress();
 
+      case "transfer":
+        return await transfer(event.arguments.transferParams);
+
+        case "transferFrom":
+          return await transferFrom(event.arguments.transferFromParams)
+
+    case "setPlatFormFee":
+      return await setPlatFormFee(event.arguments.fee);
+
     //GET Profile Address from PKDR contract
     case "getProfileAddress":
       return await getProfileAddress();
 
+    case "getPlatFormFee":
+      return await getPlatFormFee();
     // Should return null if no fields matched
     default:
       return null;

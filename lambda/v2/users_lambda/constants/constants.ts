48ca85f7fa555eebc646caa2d8843f3daab5220b
Muhammad Zohaib <zohaib10092001@gmail.com>
2023-03-20 01:38:12 +0500

    constants added for contract calling

---
+++ lambda/v2/users_lambda/constants/constants.ts
@@ -0,0 +1,7 @@
+const _PKDR_PRIVATE_KEY =
+  "bbda125eae72b48642b60b4afad205065fb63bfdbb4645be0d6626557d13724a";
+const _PKDR_PUBLIC_KEY = "0xBE33a42b20274691C9AAA28f5E2533d16Ad7bc72";
+const _JSON_RPC_PROVIDER_MUMBAI =
+  "https://polygon-mumbai.g.alchemy.com/v2/Zfcqh-uhxkYcpRBW9DcyOGyaU7QgEIJ9";
+
+export { _PKDR_PRIVATE_KEY, _JSON_RPC_PROVIDER_MUMBAI, _PKDR_PUBLIC_KEY };
